92077066e05c0d6612a2ebd36afa710826de9357
jaymin dave <jaymin4724@gmail.com>
2025-11-05 16:03:39 +0530

    dist updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.204.84.85:5000", // <-- YOUR EC2 SERVER IP
+  baseURL: "/", // <-- YOUR EC2 SERVER IP
   withCredentials: true, // This is important for cookies
 });
 
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -11,17 +11,13 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io(
-        // "http://localhost:5000", // Uncomment this for local dev if needed
-        "http://13.204.84.85:5000",
-        {
-          query: {
-            // --- CHANGED ---
-            // We now send the username as the unique ID for the socket server
-            userId: authUser.username,
-          },
-        }
-      );
+      const socket = io("/", {
+        query: {
+          // --- CHANGED ---
+          // We now send the username as the unique ID for the socket server
+          userId: authUser.username,
+        },
+      });
       SetSocket(socket);
 
       socket.on("getOnlineUsers", (users) => {
